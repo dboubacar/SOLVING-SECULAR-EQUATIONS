a0763eed4d9fed90f70236f64e6f966df6408315
boubacar <dbsidi@yahoo.fr>
2020-05-03 14:24:35 +0200

    method hybride,Middle Way et Fixed Weight

--- src/hybrid.h
+++ src/hybrid.h
@@ -1,10 +1,30 @@
+/*
+Résolution de l’équation séculaire
+  Par: Boubacar Diallo  et
+       Jeffrey  Kebey
+
+Dans le cadre du Pojet SFPN
+
+hybrid.h
+------
+
+Contient les prototypes des fonctions de hybrid.c
+*/
+
+
 #ifndef DEF_HYBRID
 #define DEF_HYBRID
 #include "param.h"
 
 
+double middleWay(const PARAM *p,double y,int k,bool useF);
+double fixedWeight(const PARAM *p,double y,int k,bool useF);
 
+double commute(const PARAM *p,double y,int k ,bool isFixe);
 
+Secular * lambda_hybrid(const PARAM * p,int k);
+
+Secular * hybrid(const PARAM *p);
 
 
 
